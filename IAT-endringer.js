1ab4499d2da9028f6173b031d6b911828606151c
T2deB <145110528+T2deB@users.noreply.github.com>
2023-10-30 10:09:34 +0100

    Update IAT-endringer.js
    Mer norsk

--- IAT-endringer.js
+++ IAT-endringer.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://cdn.jsdelivr.net/gh/t2deb/Vurdering.github.io/QualtricsMainCodeIAT.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://baranan.github.io/minno-tasks/quiat8.js'], function(APIConstructor, iatExtension){
     var API = new APIConstructor();
 
 	return iatExtension({
@@ -40,6 +40,94 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/t2deb/Vurdering.github.io/Qualtri
 
 		base_url : {//Where are your images at?
 			image : 'https://baranan.github.io/minno-tasks/images/'
-		} 
+		},
+		attribute1 :
+			{
+				name : 'Dårlige ord',
+				title : {
+					media : {word : 'Dårlige ord'},
+					css : {color:'#0000FF','font-size':'1.8em'},
+					height : 4 //Used to position the "Or" in the combined block.
+				},
+				stimulusMedia : [ //Stimuli content as PIP's media objects
+					{word: 'fryktelig'},
+					{word: 'taper'},
+					{word: 'lidelse'},
+					{word: 'såret'},
+					{word: 'horribelt'},
+					{word: 'terribelt'},
+					{word: 'motbydelig'},
+					{word: 'ondksap'}
+				],
+				//Stimulus css
+				stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+			},
+			attribute2 :
+			{
+				name : 'Gode ord',
+				title : {
+					media : {word : 'Gode ord'},
+					css : {color:'#0000FF','font-size':'1.8em'},
+					height : 4 //Used to position the "Or" in the combined block.
+				},
+				stimulusMedia : [ //Stimuli content as PIP's media objects
+					{word: 'latter'},
+					{word: 'lykkelig'},
+					{word: 'strålende'},
+					{word: 'glede'},
+					{word: 'vidunderlig'},
+					{word: 'fred'},
+					{word: 'nytelse'},
+					{word: 'kjærlighet'}
+				],
+				//Stimulus css
+				stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+			},
+			leftKeyText : 'Trykk "E" for', 
+			rightKeyText : 'Trykk "I" for', 
+			keysCss : {'font-size':'0.8em', 'font-family':'courier', color:'#000000'},
+			//Text and style for the separator between the top and bottom category labels.
+			orText : 'eller', 
+			orCss : {'font-size':'1.8em', color:'#000000'},
+			finalText : 'Trykk på mellomromstasten for å forstette til neste oppgave',
+			
+		instCategoriesPractice: '<div><p align="center" style="font-size:20px; font-family:arial">' +
+				'<font color="#000000"><u>Del blockNum av nBlocks </u><br/><br/></p>' +
+				'<p style="font-size:20px; text-align:left; vertical-align:bottom; margin-left:10px; font-family:arial">' +
+				'Legg venstre finger på <b>E</b> tasten for objekter som tilhører kategorien <font color="#336600">leftCategory</font>. ' +
+				'<br/>Legg høyre finger på <b>I</b> tasten for objekter som tilhører kategorien <font color="#336600">rightCategory</font>.<br/>' +
+				'Ett og ett objekt vil vises om gangen.<br/><br/>' +
+				'Hvis du gjør feil vil en rød <font color="#ff0000"><b>X</b></font> vises. ' +
+				'Trykk på den andre tasten for å fortsette.<br/>' +
+				'<u>Gjennomfør så raskt du kan </u> mens du er så nøyaktig som mulig.<br/><br/></p>'+
+				'<p align="center">Trykk på <b>space bar</b> når du er klar til å starte.</font></p></div>',
+				
+		instFirstCombined : '<div><p align="center" style="font-size:20px; font-family:arial">' +
+				'<font color="#000000"><u>Del blockNum av nBlocks </u><br/><br/></p>' +
+				'<p style="font-size:20px; text-align:left; vertical-align:bottom; margin-left:10px; font-family:arial">' +
+				'Bruk <b>E</b> tasten for <font color="#336600">leftCategory</font> og for <font color="#0000ff">leftAttribute</font>.<br/>' +
+				'Bruk <b>I</b> tasten for <font color="#336600">rightCategory</font> og for  <font color="#0000ff">rightAttribute</font>.<br/>' +
+				'Hvert obejkt tilhører bare en kategori.<br/><br/>' +
+				'Hvis du gjør feil vil en rød <font color="#ff0000"><b>X</b></font> vises. ' +
+				'Trykk på den andre tasten for å fortsette.<br/>' + 
+				'<u>Gjennomfør så raskt du kan </u> mens du er så nøyaktig som mulig.<br/><br/></p>' +
+				'<p align="center">Trykk på <b>space bar</b> når du er klar til å starte.</font></p></div>',
+		instSecondCombined : '<div><p align="center" style="font-size:20px; font-family:arial">' +
+				'<font color="#000000"><u>Del blockNum av nBlocks </u><br/><br/></p>' +
+				'<p style="font-size:20px; text-align:left; vertical-align:bottom; margin-left:10px; font-family:arial">' +
+				'Dette er det samme som den forrige delen.<br/>' +
+				'Bruk <b>E</b> tasten for <font color="#336600">leftCategory</font> og for <font color="#0000ff">leftAttribute</font>.<br/>' +
+				'Bruk <b>I</b> tasten for <font color="#336600">rightCategory</font> og for  <font color="#0000ff">rightAttribute</font>.<br/>' +
+				'Hvert objekt tilhører bare en kategori.<br/><br/>' +
+				'<u>Gjennomfør så raskt du kan </u> mens du er så nøyaktig som mulig.<br/><br/></p>' +
+				'<p align="center">Trykk på <b>space bar</b> når du er klar til å starte.</font></p></div>',
+		instSwitchCategories : '<div><p align="center" style="font-size:20px; font-family:arial">' +
+				'<font color="#000000"><u>Del blockNum av nBlocks </u><br/><br/></p>' +
+				'<p style="font-size:20px; text-align:left; vertical-align:bottom; margin-left:10px; font-family:arial">' +
+				'<b>Pass på, kategoriene har byttet posisjon!</b><br/>' +
+				'Legg venstre finger på <b>E</b> tasten for <font color="#336600">leftCategory</font>.<br/>' +
+				'Legg høyre finger på <b>I</b> tasten for <font color="#336600">rightCategory</font>.<br/><br/>' +
+				'<u>Gjennomfør så raskt du kan </u> mens du er så nøyaktig som mulig.<br/><br/></p>' +
+				'<p align="center">Trykk på <b>space bar</b> når du er klar til å starte.</font></p></div>',				
 	});
 });
